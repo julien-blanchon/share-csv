3f01dd1b388d1fe2c6bade5eff43ccd29707bc30
Julien Blanchon <julien@blanchon.cc>
2024-09-13 16:46:13 +0200

    Initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { Badge } from "@/components/ui/badge";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -28,7 +29,17 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <main className="container mx-auto flex min-h-screen flex-col gap-4 px-2 py-4 md:px-4 md:py-8">
+        <div className="relative flex flex-1 flex-col rounded-lg border border-border/50 bg-background/50 p-4 backdrop-blur-[2px] sm:p-8">
+          {children}
+          <Badge
+            variant="outline"
+            className="absolute -top-2.5 left-4 bg-background sm:left-8"
+          >
+            Work in progress
+          </Badge>
+        </div>
+    </main>
       </body>
     </html>
   );
