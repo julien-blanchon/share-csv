c9f72c5cfee6bef1c774919b3c6f038b0d79666b
IronJayx <iron.jayxxx@gmail.com>
2024-09-15 14:56:02 +0200

    dirty logo and favicon

--- components/custom/header.tsx
+++ components/custom/header.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import { LoginDialog } from "./login"
 import { RegisterDialog } from "./register"
 import { createClient } from '@/utils/supabase/server'
@@ -8,15 +9,19 @@ export default async function Component() {
   const { data, error } = await supabase.auth.getUser()
 
   return (
-    <div className="ml-auto flex gap-2">
-      {data?.user ? (
-        <AvatarDropdown user={data.user} />
-      ) : (
-        <div className="flex gap-2">
-          <LoginDialog />
-          <RegisterDialog />
-        </div>
-      )}
+    <div className="flex items-center justify-between w-full">
+      {/* TODO: Fix display, height has no impact right now and it overlays with toggle*/}
+      <Image src="/logo_name.svg" alt="Logo" width="100" height={450} />
+      <div className="ml-auto flex gap-2">
+        {data?.user ? (
+          <AvatarDropdown user={data.user} />
+        ) : (
+          <div className="flex gap-2">
+            <LoginDialog />
+            <RegisterDialog />
+          </div>
+        )}
+      </div>
     </div>
   )
 }
